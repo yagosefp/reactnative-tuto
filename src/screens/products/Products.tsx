db475a08fae5fd2f4aca193f77335b8e2f7be52a
Roi Dopazo <roidopazo@gmail.com>
2021-12-06 14:14:15 +0100

    [Exercise2] Paso 2

--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -1,9 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Button, Text } from 'react-native-elements';
 import styles from './product-styles';
 import Counter from '../../components/counter/Counter';
+import ProductsService from '../../services/ProductsService';
 
 const Products = () => {
   const navigation = useNavigation<any>();
@@ -11,6 +12,15 @@ const Products = () => {
   const onPressButton = () => {
     navigation.navigate('product-details');
   };
+
+  useEffect(() => {
+    const getAsyncProducts = async () => {
+      const result = await ProductsService.getProducts();
+      console.log(result.data);
+    };
+
+    getAsyncProducts();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
